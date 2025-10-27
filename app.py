3ee04afc2f871aa36ee437b122c57dfd01635e2f
livdinonno <krx5sa@virginia.edu>
2025-10-27 14:07:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 
 st.set_page_config(page_title="KSTAR Visualizer", layout="wide", initial_sidebar_state = "expanded")
 
