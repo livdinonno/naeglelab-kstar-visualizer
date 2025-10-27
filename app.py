93667311e5b55c131dd8daef9dfeae84d4c56454
livdinonno <krx5sa@virginia.edu>
2025-10-27 14:18:10 -0400

    Refactor imports and update markdown message

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
-#import seaborn as sns
 
-st.set_page_config(page_title="KSTAR Visualizer", layout="wide", initial_sidebar_state = "expanded")
+try:
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+except Exception:
+    pass
+
+st.set_page_config(page_title="KSTAR Visualizer", layout="wide", initial_sidebar_state="expanded")
 
 def home_page():
     st.title("Publication on Phosphotyrosine Profiling")
-    st.markdown("""Welcome to the KSTAR Visualizer. This site allows you to visualize your data after processing it through the KSTAR algorithm. It also links relevant publications and tutorials.""")
+    st.markdown("Welcome to the KSTAR Visualizer. This site allows you to visualize your data after processing it through the KSTAR algorithm. It also links relevant publications and tutorials.")
 
+if __name__ == "__main__":
+    home_page()
